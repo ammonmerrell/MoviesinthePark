441fb63eb5d0f779c82d0354bc2c3d5f9a865393
ammonmerrell <amerrell@byupathway.edu>
2025-11-29 11:56:55 -0700

    added css and main

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+const a = document.querySelector(".list");
+a.innerHTML = ``;
+function addProductToList(event) {
+  const eventList = getLocalStorage("so-events") || [];
+  eventList.push(event);
+  setLocalStorage("so-events", eventList);
+}
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-events") || [];
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  a.innerHTML = htmlItems.join("");
+}
+
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  
+    <h2 class="card__name">${item.Name}</h2>
+  <button id="view" dataset="${item.Name}">view</button>
+</li>`;
+  return newItem;
+}
+let ev = {
+  Name: "newEvent",
+  Date: "N/A",
+  FinalMovie: "???",
+  MovieList: ["The+Peanuts+movie", "Up", "luca", "Ice+Age"],
+};
+
+function removeFromCart() {
+  const buttons = document.querySelectorAll("#view");
+  buttons.forEach((button) =>
+    button.addEventListener("click", function (event) {
+      const itemId = event.target.getAttribute("dataset");
+      let cartItems = getLocalStorage("so-cart") || [];
+      cartItems.splice(itemId, 1);
+      localStorage.setItem("so-cart", JSON.stringify(cartItems));
+      console.log(itemId);
+      renderCartContents();
+    }),
+  );
+}
+
+// addProductToList(ev)
+renderCartContents();
+removeFromCart();

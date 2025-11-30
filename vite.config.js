aef68a152e40d1a9e830881c3f59600d994d8436
ammonmerrell <amerrell@byupathway.edu>
2025-11-29 17:26:13 -0700

    added netfixy link and removed vite link

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        
-        
-        
-        
-        
       },
     },
   },

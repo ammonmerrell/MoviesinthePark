1aa5445ef3c6fb3437cc249f3a425700c2fc827d
ammonmerrell <amerrell@byupathway.edu>
2025-11-29 20:09:40 -0700

    npm run format

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,43 +1,42 @@
 const title = document.querySelector(".name");
-title.innerHTML = "ad"
-const movie = document.querySelector("#movie")
-const pic = document.querySelector("#pic")
-const cap = document.querySelector("#caption")
-const url = 'https://www.omdbapi.com/?t=up&apikey=3cf352d ';
+title.innerHTML = "ad";
+const movie = document.querySelector("#movie");
+const pic = document.querySelector("#pic");
+const cap = document.querySelector("#caption");
+const url = "https://www.omdbapi.com/?t=up&apikey=3cf352d";
 
 // const weatherTemp2 = document.querySelector('#movie');
 // const weatherIcon2 = document.querySelector('#pic');
 // const captionDest2 = document.querySelector('#caption');
 
-
 async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            displayResults(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayResults(data) {
-    movie.innerHTML = `${data.Title}`;
-    
-    const desc = data.Poster;
-    pic.setAttribute('src', desc);
-    pic.setAttribute('alt', data.Title);
-    cap.textContent = `${data.Plot}`;
+  movie.innerHTML = `${data.Title}`;
 
-    // weatherTemp2.innerHTML = `${data.Title}&deg;F`;
-    // const desc = data.Poster;
-    // console.log(data.Title)
-    // pic.setAttribute('src', desc);
-    // pic.setAttribute('alt', data.Title);
-    // cap.textContent = data.Plot;
+  const desc = data.Poster;
+  pic.setAttribute("src", desc);
+  pic.setAttribute("alt", data.Title);
+  cap.textContent = `${data.Plot}`;
+
+  // weatherTemp2.innerHTML = `${data.Title}&deg;F`;
+  // const desc = data.Poster;
+  // console.log(data.Title)
+  // pic.setAttribute('src', desc);
+  // pic.setAttribute('alt', data.Title);
+  // cap.textContent = data.Plot;
 }
-apiFetch()
+apiFetch();

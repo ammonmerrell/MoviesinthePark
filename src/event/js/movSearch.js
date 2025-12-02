a436f44eda858e30e5d4fd1a959151c2096b9fa3
ammonmerrell <amerrell@byupathway.edu>
2025-12-02 09:28:26 -0700

    creared local js file and search html

---
+++ src/event/js/movSearch.js
@@ -0,0 +1,41 @@
+const title = document.querySelector(".name");
+title.innerHTML = "Movie";
+const display = document.querySelector("#display");
+const pic = document.querySelector("#pic");
+const cap = document.querySelector("#caption");
+const button = document.querySelector("#search")
+
+const getString = window.location.search;
+const mySearch = new URLSearchParams(getString)
+let movie = mySearch.get("movie");
+let long = mySearch.get("desc");
+const url = `https://www.omdbapi.com/?t=${movie}&apikey=3cf352d${long}`;
+
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    display.innerHTML = `${data.Title}`;
+    console.log(pic)
+    const desc = data.Poster;
+    pic.setAttribute("src", desc);
+    pic.setAttribute("alt", data.Title);
+    cap.textContent = `${data.Plot}`;
+
+
+}
+apiFetch();

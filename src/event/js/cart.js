ff0b9d5fd5bbb34166bc2b3148e60edd7d7d6086
ammonmerrell <amerrell@byupathway.edu>
2025-12-03 22:17:35 -0700

    changed p and displayed event  info.

--- src/event/js/cart.js
+++ src/event/js/cart.js
@@ -1,1 +1,9 @@
 const title = document.querySelector(".name");
+const info = document.querySelector(".info");
+// function imported/copied from utils
+let evmov = JSON.parse(localStorage.getItem("event"));
+console.log(evmov.Name);
+info.innerHTML = `<p>Event: ${evmov.Name}</p>
+<p>Date: ${evmov.Date}</p>
+<p>Final Movie: ${evmov.FinaMovie}</p>
+<p>Movie Options: ${evmov.MovieList}</p>`;

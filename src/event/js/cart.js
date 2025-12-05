261804144b3514092e08633c9c934a48052a2d71
ammonmerrell <amerrell@byupathway.edu>
2025-12-05 10:53:32 -0700

    updated to read correctly?

--- src/event/js/cart.js
+++ src/event/js/cart.js
@@ -2,8 +2,8 @@ const title = document.querySelector(".name");
 const info = document.querySelector(".info");
 // function imported/copied from utils
 let evmov = JSON.parse(localStorage.getItem("event"));
-console.log(evmov[0].Name);
-info.innerHTML = `<p>Event: ${evmov[0].Name}</p>
-<p>Date: ${evmov[0].Date}</p>
-<p>Final Movie: ${evmov[0].FinaMovie}</p>
-<p>Movie Options: ${evmov[0].MovieList}</p>`;
+console.log(evmov.FinalMovie);
+info.innerHTML = `<p>Event: ${evmov.Name}</p>
+<p>Date: ${evmov.Date}</p>
+<p>Final Movie: ${evmov.FinalMovie}</p>
+<p>Movie Options: ${evmov.MovieList}</p>`;

bf0a4c20c7d3460ad446277d30dfd10b40e4055c
ammonmerrell <amerrell@byupathway.edu>
2025-12-08 16:40:59 -0700

    added back button, and button fuctionality

--- src/event/js/cart.js
+++ src/event/js/cart.js
@@ -1,8 +1,15 @@
 const title = document.querySelector(".name");
 const info = document.querySelector(".info");
+const back = document.querySelector(".back")
 // function imported/copied from utils
 let evmov = JSON.parse(localStorage.getItem("event"));
 info.innerHTML = `<p>Event: ${evmov.Name}</p>
 <p>Date: ${evmov.date}</p>
 <p>Final Movie: ${evmov.FinalMovie}</p>
 <p>Movie Options: ${evmov.MovieList}</p>`;
+
+back.innerHTML = `<input type=submit value="Back" id="back"/>`
+
+back.addEventListener("click", () => {
+    window.location.href = "src/"
+})

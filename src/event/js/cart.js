fba8a302c3343112329b2a900367aa1772f17f3e
ammonmerrell <amerrell@byupathway.edu>
2025-12-09 08:48:41 -0700

    added document event

--- src/event/js/cart.js
+++ src/event/js/cart.js
@@ -2,14 +2,15 @@ import { getLocalStorage } from "../../js/utils.mjs";
 const title = document.querySelector(".name");
 const info = document.querySelector(".info");
 const back = document.querySelector(".back");
-// function imported/copied from utils
-let evmov = getLocalStorage("event");
-info.innerHTML = `<p>Event: ${evmov.Name}</p>
+document.addEventListener("DOMContentLoaded", () => {
+  let evmov = getLocalStorage("event");
+  info.innerHTML = `<p>Event: ${evmov.Name}</p>
 <p>Date: ${evmov.date}</p>
 <p>Final Movie: ${evmov.FinalMovie}</p>
 <p>Movie Options: ${evmov.MovieList}</p>`;
+  back.innerHTML = `<input type=submit value="Back" id="back"/>`;
+});
 
-back.innerHTML = `<input type=submit value="Back" id="back"/>`;
 
 back.addEventListener("click", () => {
   window.location.href = "../../index.html";

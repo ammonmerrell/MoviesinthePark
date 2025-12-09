c9a7e47d2e8c968738a19febad1ec3e46003d878
ammonmerrell <amerrell@byupathway.edu>
2025-12-09 15:12:09 -0700

    added animaction and other finishng touches

--- src/event/js/cart.js
+++ src/event/js/cart.js
@@ -1,8 +1,9 @@
-import { getLocalStorage } from "../../js/utils.mjs";
+import { getLocalStorage, setLocalStorage } from "../../js/utils.mjs";
+
 const title = document.querySelector(".name");
 const info = document.querySelector(".info");
 const back = document.querySelector(".back");
-
+const final = document.querySelector(".final");
 document.addEventListener("DOMContentLoaded", () => {
   let evmov = getLocalStorage("event");
   info.innerHTML = `<p>Event: ${evmov.Name}</p>
@@ -10,8 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
 <p>Final Movie: ${evmov.FinalMovie}</p>
 <p>Movie Options: ${evmov.MovieList}</p>`;
   back.innerHTML = `<input type=submit value="Back" id="back"/>`;
+  final.innerHTML = `<input type=submit value="Select Movie" id="select" />`;
 });
+
+// info.addEventListener("mouseover", () => {
+//   info.classList.toggle("in");
+// })
 
 back.addEventListener("click", () => {
   window.location.href = "../../index.html";
+});
+final.addEventListener("click", () => {
+  let evmov = getLocalStorage("event");
+  let selection = Math.floor(Math.random() * evmov.MovieList.length);
+  evmov.FinalMovie = evmov.MovieList[selection];
+  setLocalStorage("event", evmov);
+  setLocalStorage("so-events", evmov);
+  info.classList.toggle("in");
+  // window.location.href = "/event/index.html";
 });
